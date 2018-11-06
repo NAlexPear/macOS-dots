5d81e61d1a721c586bbd3cb5a1af451ed0049c2f
Alex Pearson <alex@joinvolley.com>
2018-11-06 09:53:17 -0600

    :wrench: Set up oh-my-zsh installation in .config directory instead of /Users/apearson

--- .zshrc
+++ .zshrc
@@ -2,7 +2,7 @@
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-export ZSH=/Users/alexpearson/.oh-my-zsh
+export ZSH="$HOME/.config/oh-my-zsh"
 
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
@@ -84,13 +84,15 @@ source $ZSH/oh-my-zsh.sh
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 alias gs="git status"
-alias vim=/usr/local/bin/vim
+alias vim=nvim
 
 export DEFAULT_USER=alexpearson
 
 export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
 
-export PATH="/bin:$HOME/.cargo/bin:$PATH"
+export PATH="/usr/local/opt/ruby/bin:/usr/local/opt/ruby:/bin:$HOME/.cargo/bin:$PATH"
+export GEM_HOME="/usr/local/opt/ruby/lib/ruby/gems/2.5.0"
+export GEM_PATH="/usr/local/opt/ruby/lib/ruby/gems/2.5.0"
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
--- init.sh
+++ init.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 # force rename to proper directory
-if [ ! "$PWD" == "$HOME/config" ]; then
-  mv "$PWD" ~/.config
+if [ ! "$PWD" == "$HOME/.config" ]; then
+  mv "$PWD" ~/.config/
 fi
 
 # initialize submodules
 git submodule init
 git submodule update --remote
-
-# symlink config files
-ln -s ~/.*rc ~
 
 # set up homebrew if needed
 if [ ! $(which brew) ]; then
@@ -19,7 +16,7 @@ fi
 
 # forumulae
 brew upgrade
-brew install ruby git fd bat neovim
+brew install ruby git fd bat neovim ripgrep htop
 brew install koekeishiya/formulae/skhd
 
 # taps
@@ -33,11 +30,21 @@ brew services start chunkwm
 brew services start skhd
 
 # terminal configuration
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+export ZSH="$HOME/.config/oh-my-zsh"; sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# symlink config files
+rm ~/.zshrc
+ln -sf ~/.config/.*rc ~
+
+# rust
+if [ ! $(which cargo) ]; then
+  curl https://sh.rustup.rs -sSf | sh
+fi
 
 # node
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 source ~/.zshrc
+nvm install node
 
 # neovim providers
 pip3 install --user neovim
@@ -46,3 +53,7 @@ gem install neovim
 
 # iTerm2
 cp ~/.config/iTerm2/com.googlecode.iterm2.plist ~/Library/Preferences/
+
+# Exit message
+echo -e "\n==========\n"
+echo "Configuration complete! Please restart your terminal"
